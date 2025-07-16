c732834c7196a0904f89a913f48c5e1d60c6993a
Victor Nogueira <vgdsno@gmail.com>
2025-07-16 15:43:23 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.OpenApi.Models;
+using ProdutoApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ProdutoRepository>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Produto API",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
